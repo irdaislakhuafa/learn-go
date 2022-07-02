dd7a4378d7690ce23cb253843b257ea5a68e7b1e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-02 14:44:25 +0700

    learn comment

---
+++ 2_Comment/go.mod
@@ -0,0 +1,3 @@
+module github.com/irdaislakhuafa/learn-go/2_Comment
+
+go 1.18
---
+++ 2_Comment/main.go
@@ -0,0 +1,14 @@
+package comment
+
+import "fmt"
+
+func main() {
+	// this id comment
+	// this comment using single line comment
+	// print "Hello World" to console
+	fmt.Println("Hello World")
+
+	/*
+		this is multiline comment
+	*/
+}
