0057fa691bd31aa8c175375606bb7f6ec87e48f9
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-05 21:36:20 +0700

    try create variable with make()

--- 3_Variable/main.go
+++ 3_Variable/main.go
@@ -30,4 +30,19 @@ func main() {
 	*gf = "ana ardani"
 	fmt.Println("GF :", *gf)
 
+	// create variable with make() keyword
+	// only can be used in (slice, array, map, channel)
+	hobbies1 := make(map[any]any)
+	hobbies2 := map[any]any{} // same like above
+	var a map[any]any         // nil/unalocated value
+
+	hobbies1["irda"] = "Mengoding"
+	hobbies2["someone"] = "Menyanyi"
+	// a["anything"] = "anything" //panic
+	a = map[any]any{}
+	a["anything"] = "anything" //success
+
+	fmt.Printf("Hobbies 1: %+v\n", hobbies1)
+	fmt.Printf("Hobbies 2: %+v\n", hobbies2)
+	fmt.Printf("anything: %+v\n", a)
 }
