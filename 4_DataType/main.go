0a60c8d2cf9f14aba4c8424dc831aa25e3a098cf
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-05 23:15:13 +0700

    create some comment

---
+++ 4_DataType/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	/*
+		uint8 = 0 => 255
+		uit16 = 0 => 65535
+		uit32 = 0 => 4294967295
+		uint64 = 0 => 18446744073709551615
+		uint = same as uint32 or uint64 depends value
+		int8 = 128 => 127
+		int16 = -32768 => 32767
+		int32 = -2147483648 => 2147483647
+		int64 = -9223372036854775808 => 9223372036854775807
+		int = same as int32 or int64 depends value
+		rune = same as int32
+
+	*/
+}
