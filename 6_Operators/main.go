f60b290dc0f7b21038fc5eae19c5f0245cc3e8a9
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-09 01:12:41 +0700

    use "increase operator

---
+++ 6_Operators/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// try use some operators in Go Language
+
+	// aritmatics operators (+, -, /, *, %)
+	increase := 2 + 2
+	fmt.Println("2 + 2 :", increase)
+}
