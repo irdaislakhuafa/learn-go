8de6816624121ac42981db5827c2c6eceb21b067
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-10 20:50:27 +0700

    use `for` keyword to looping with `while` keyword style

--- 8_Looping/main.go
+++ 8_Looping/main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	// looping with `for` example 1
 	for i := 0; i < 5; i++ {
 		fmt.Println("Number :", i)
 	}
+	line()
 
+	// for with `while` style
+	i := 0
+	for i < 5 {
+		fmt.Println("Number :", i)
+	}
+}
+
+func line() {
+	fmt.Println(strings.Repeat("=", 20))
 }
