a403ffc05415aff35fb3be5b1306cb8e7dcb1dd6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-09-23 21:29:12 +0700

    trying to learn what difference between slice and array

--- 10_Slice/main.go
+++ 10_Slice/main.go
@@ -11,7 +11,7 @@ func main() {
 	arrayFruits := [...]string{"banana", "apple"}
 
 	// try use "append()"
-	sliceFruits = append(sliceFruits, "lecy")
+	// sliceFruits = append(sliceFruits, "lecy") // can use "append()" in slice
 	// arrayFruits = append(arrayFruits, "lecy") // can't use "append()" in array
 
 	fmt.Println("Slice Fruits:", sliceFruits)
@@ -20,7 +20,7 @@ func main() {
 	fmt.Println("Array Fruits[0]:", arrayFruits[0])
 	fmt.Println("Slice Fruits[1]:", sliceFruits[1])
 	fmt.Println("Array Fruits[1]:", arrayFruits[1])
-	fmt.Println("Slice Fruits[2]:", sliceFruits[2]) // don't have error notif but panic while code is running
+	// fmt.Println("Slice Fruits[2]:", sliceFruits[2]) // don't have error notif but panic while code is running
 	// fmt.Println("Array Fruits[0]:", arrayFruits[2]) // have error notif
 }
 
