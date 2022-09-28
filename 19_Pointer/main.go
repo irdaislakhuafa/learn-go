ad2b701a034ce36115888ed7e14a4bdfc7b4ce16
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-09-29 02:37:08 +0700

    pointer as parameter

--- 19_Pointer/main.go
+++ 19_Pointer/main.go
@@ -7,28 +7,42 @@ import (
 )
 
 func main() {
-	Line("pointer implementation")
-	// create pointer variables
-	var number *int
-	var name *string
+	{
+		printValueAndAddress := func(a *int, b **int) {
+			fmt.Printf("numberA (Value)\t\t: %v\n", *a)
+			fmt.Printf("numberA (Address)\t: %v\n", a)
+			fmt.Printf("numberB (Value)\t\t: %v\n", **b)
+			fmt.Printf("numberB (Address)\t: %v\n", *b)
+		}
 
-	fmt.Printf("number: %v\n", number) // nil
-	fmt.Printf("name: %v\n", name)     // nil
+		Line("pointer implementation")
+		// create pointer variables
+		var number *int
+		var name *string
 
-	Line2("implementation")
-	var numberA int = 2
-	var numberB *int = &numberA
-	printValueAndAddress(&numberA, &numberB)
+		fmt.Printf("number: %v\n", number) // nil
+		fmt.Printf("name: %v\n", name)     // nil
 
-	Line("pointer value change effect")
-	numberA = 10
-	printValueAndAddress(&numberA, &numberB)
-}
+		Line2("implementation")
+		var numberA int = 2
+		var numberB *int = &numberA
+		printValueAndAddress(&numberA, &numberB)
 
-func printValueAndAddress(a *int, b **int) {
-	fmt.Printf("numberA (Value)\t\t: %v\n", *a)
-	fmt.Printf("numberA (Address)\t: %v\n", a)
-	fmt.Printf("numberB (Value)\t\t: %v\n", **b)
-	fmt.Printf("numberB (Address)\t: %v\n", *b)
+		Line("pointer value change effect")
+		numberA = 10
+		printValueAndAddress(&numberA, &numberB)
+	}
 
+	{
+		Line("pointer as parameter")
+		change := func(current *int, nextValue int) {
+			*current = nextValue
+		}
+
+		number := 4
+		fmt.Printf("Before: %v\n", number)
+
+		change(&number, 10)
+		fmt.Printf("After: %v\n", number)
+	}
 }
