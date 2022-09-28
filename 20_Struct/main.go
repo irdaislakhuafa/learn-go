e8107677d376fecc855ff5789151df81262fb73f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-09-29 02:49:32 +0700

    create 3 struct with different way

--- 20_Struct/main.go
+++ 20_Struct/main.go
@@ -27,5 +27,24 @@ func main() {
 		fmt.Printf("student1.name\t: %v\n", student1.name)
 		fmt.Printf("student1.grade\t: %v\n", student1.grade)
 		fmt.Printf("student{}\t: %+v\n", student{})
+
+		utils.Line("create 3 struct variable with different way")
+		// 1
+		v1 := student{}
+		v1.name = "me"
+		v1.grade = 1
+
+		// 2
+		v2 := student{"you", 2}
+
+		// 3
+		v3 := student{
+			name:  "we",
+			grade: 3,
+		}
+
+		fmt.Printf("v1: %+v\n", v1)
+		fmt.Printf("v2: %+v\n", v2)
+		fmt.Printf("v3: %+v\n", v3)
 	}
 }
