0f4c32c24a9e20a466d8226c115dcf0786d0cb76
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-09-30 03:43:33 +0700

    embedded interface (like inheritance in OOP language)

--- 23_Interface/main.go
+++ 23_Interface/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/irdaislakhuafa/learn-go/23_Interface/embeddedinterface"
 	"github.com/irdaislakhuafa/learn-go/23_Interface/examinterface"
 	"github.com/irdaislakhuafa/learn-go/utils"
 )
@@ -25,6 +26,14 @@ func main() {
 	}
 
 	{
+		utils.Line("embedded interface (like inheritance in OOP language)")
+		var geometry embeddedinterface.Count = &embeddedinterface.Cube{
+			Side: 4.0,
+		}
 
+		utils.Line2(fmt.Sprintf("cube: %+v", geometry))
+		fmt.Println("large\t:", geometry.Large())
+		fmt.Println("around\t:", geometry.Around())
+		fmt.Println("volume\t:", geometry.Volume())
 	}
 }
