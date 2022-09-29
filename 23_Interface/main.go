6e2f641bb478ee40161a658aea147056f4db781a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-09-30 03:29:40 +0700

    interface implementation

--- 23_Interface/main.go
+++ 23_Interface/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/irdaislakhuafa/learn-go/23_Interface/examinterface"
+	"github.com/irdaislakhuafa/learn-go/utils"
+)
+
+func main() {
+	utils.Line("interface implementation")
+	var twoDimentionalFigure examinterface.Count
+
+	twoDimentionalFigure = &examinterface.Rectangle{Side: 10.0}
+	utils.Line2(fmt.Sprintf("rectangle: %+v", twoDimentionalFigure))
+	fmt.Println("rectangle large\t\t:", twoDimentionalFigure.Large())
+	fmt.Println("rectangle around\t:", twoDimentionalFigure.Around())
+
+	twoDimentionalFigure = &examinterface.Circle{Diameter: 14.0}
+	utils.Line2(fmt.Sprintf("circle: %+v", twoDimentionalFigure))
+	fmt.Println("circle large\t\t:", twoDimentionalFigure.Large())
+	fmt.Println("circle around\t\t:", twoDimentionalFigure.Around())
+	fmt.Println("circle fingers\t\t:", twoDimentionalFigure.(*examinterface.Circle).Fingers())
 }
