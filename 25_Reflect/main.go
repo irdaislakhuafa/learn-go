16484a8252d99584a909dbe4e45c3283a294f7c9
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-10-08 16:01:42 +0700

    initialize reflect section

---
+++ 25_Reflect/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	/*
+		reflect is variable inspection technique, used to get information from a variable and manipulate a variable
+		reflect have many function but 2 more important function
+		- reflect.ValueOf() : will return object with type reflect.Value, contains all information about value of variable
+		- reflect.TypeOf() : will return object with type reflect.Type, contains all information about type of variable
+	*/
+}
